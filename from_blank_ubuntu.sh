9037a6db39f5c83a3232dd8b32fae3f0c1093388
Sravan Vankina <spvankina@gmail.com>
2015-07-21 22:41:04 -0400

    adding setup stuff for starting up ubuntu

---
+++ from_blank_ubuntu.sh
@@ -0,0 +1,70 @@
+#Libraries required by a lot of stuff
+sudo apt-get install automake libevent-dev libncurses5-dev gnome-tweak-tool libncursesw5-dev
+
+#Setup git
+sudo apt-get install git
+
+#Configure git
+git config --global user.name "Sravan Vankina"
+git config --global user.email "spvankina@gmail.com"
+git config --global push.default current
+
+#Install python-dev
+sudo apt-get install python-dev
+
+cd $HOME
+
+#Setup my custom builds
+mkdir my_apps
+cd my_apps
+
+#Install pip
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+rm get-pip.py
+
+#Install virtualenvwrapper
+sudo pip install virtualenvwrapper
+
+#Install powerline
+pip install --user powerline-status
+
+#Install vim-gnome
+sudo apt-get install vim-gnome
+
+#Setup NeoBundle
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+sh install.sh
+vim +NeoBundleInstall +qall
+rm install.sh
+
+
+#Install tmux
+wget https://github.com/tmux/tmux/releases/download/2.0/tmux-2.0.tar.gz
+tar -xvf tmux-2.0.tar.gz
+cd tmux-2.0
+./configure --prefix=$HOME/.local
+make && make install
+cd ../
+rm -rf tmux-*
+
+
+#Get htop from github, install, and cleanup
+git clone https://github.com/hishamhm/htop.git
+cd htop
+./autogen.sh
+./configure --prefix=/home/svankina/.local
+make && make install
+cd ../
+rm -rf htop
+
+cd $HOME
+
+mv $HOME/.bashrc $HOME/bashrc_backup
+#Start setting up workspace
+mkdir ~/wksp
+cd wksp
+git clone git@github.com:svankina/dotfiles.git
+sh dotfiles/setup.sh
+touch $HOME/.bash_aliases
+
--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #mv ~/.bashrc ~/bashrc_bak
 
 ln -s /home/svankina/wksp/dotfiles/.bashrc /home/svankina/.bashrc
-ln -s /home/svankina/wksp/dotfiles/.vimrc /home/svankina/.vimrc 
+ln -s /home/svankina/wksp/dotfiles/.vimrc /home/svankina/.vimrc
 ln -s /home/svankina/wksp/dotfiles/.tmux.conf /home/svankina/.tmux.conf
 ln -s /home/svankina/wksp/dotfiles/.pythonrc /home/svankina/.pythonrc
 ln -s /home/svankina/wksp/dotfiles/.pdbrc /home/svankina/.pdbrc
