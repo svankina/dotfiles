b38152e1dd536bd6847e36e45bb5035040fe1698
Sravan Vankina <spvankina@gmail.com>
2015-07-23 16:31:17 -0400

    updating setup to add link for personal

--- setup.sh
+++ setup.sh
@@ -7,5 +7,6 @@ ln -s /home/svankina/wksp/dotfiles/.pythonrc /home/svankina/.pythonrc
 ln -s /home/svankina/wksp/dotfiles/.pdbrc /home/svankina/.pdbrc
 ln -s /home/svankina/wksp/dotfiles/.zshrc /home/svankina/.zshrc
 ln -s /home/svankina/wksp/dotfiles/.bash_aliases /home/svankina/.bash_aliases
+ln -s /home/svankina/wksp/dotfiles/.bash_personal /home/svankina/.bash_personal
 
 
