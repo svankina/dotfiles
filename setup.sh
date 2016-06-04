5ab02ee720684b7bb2d9b70e9cddcc1ba5a8b2fd
Sravan Vankina <spvankina@gmail.com>
2016-06-04 18:10:58 +0530

    removing basshrc from setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 mv ~/.bashrc ~/bashrc_bak
 
-ln -s /home/svankina/wksp/dotfiles/.bashrc /home/svankina/.bashrc
 ln -s /home/svankina/wksp/dotfiles/.vimrc /home/svankina/.vimrc
 ln -s /home/svankina/wksp/dotfiles/.tmux.conf /home/svankina/.tmux.conf
 ln -s /home/svankina/wksp/dotfiles/.pythonrc /home/svankina/.pythonrc
