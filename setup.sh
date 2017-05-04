acb05016902cbc0123697977d555f4bbf5a95d6e
Sravan Vankina <spvankina@gmail.com>
2017-05-04 18:22:25 -0400

    fixed setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mv ~/.bashrc ~/bashrc_bak
 
-ln -s $HOME/wksp/dotfiles/.vimrc /home/svankina/.vimrc
-ln -s $HOME/wksp/dotfiles/.tmux.conf /home/svankina/.tmux.conf
-ln -s $HOME/wksp/dotfiles/.pythonrc /home/svankina/.pythonrc
-ln -s $HOME/wksp/dotfiles/.pdbrc /home/svankina/.pdbrc
-ln -s $HOME/wksp/dotfiles/.zshrc /home/svankina/.zshrc
-ln -s $HOME/wksp/dotfiles/.bash_aliases /home/svankina/.bash_aliases
-ln -s $HOME/wksp/dotfiles/.bash_personal /home/svankina/.bash_personal
+ln -s $HOME/wksp/dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/wksp/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/wksp/dotfiles/.pythonrc $HOME/.pythonrc
+ln -s $HOME/wksp/dotfiles/.pdbrc $HOME/.pdbrc
+ln -s $HOME/wksp/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/wksp/dotfiles/.bash_aliases $HOME/.bash_aliases
+ln -s $HOME/wksp/dotfiles/.bash_personal $HOME/.bash_personal
 
 
