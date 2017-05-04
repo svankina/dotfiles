31b0fdaddcca81d41082778ab958d9d46fd14604
Sravan Vankina <spvankina@gmail.com>
2017-05-04 18:20:45 -0400

    updating setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mv ~/.bashrc ~/bashrc_bak
 
-ln -s /home/svankina/wksp/dotfiles/.vimrc /home/svankina/.vimrc
-ln -s /home/svankina/wksp/dotfiles/.tmux.conf /home/svankina/.tmux.conf
-ln -s /home/svankina/wksp/dotfiles/.pythonrc /home/svankina/.pythonrc
-ln -s /home/svankina/wksp/dotfiles/.pdbrc /home/svankina/.pdbrc
-ln -s /home/svankina/wksp/dotfiles/.zshrc /home/svankina/.zshrc
-ln -s /home/svankina/wksp/dotfiles/.bash_aliases /home/svankina/.bash_aliases
-ln -s /home/svankina/wksp/dotfiles/.bash_personal /home/svankina/.bash_personal
+ln -s $HOME/wksp/dotfiles/.vimrc /home/svankina/.vimrc
+ln -s $HOME/wksp/dotfiles/.tmux.conf /home/svankina/.tmux.conf
+ln -s $HOME/wksp/dotfiles/.pythonrc /home/svankina/.pythonrc
+ln -s $HOME/wksp/dotfiles/.pdbrc /home/svankina/.pdbrc
+ln -s $HOME/wksp/dotfiles/.zshrc /home/svankina/.zshrc
+ln -s $HOME/wksp/dotfiles/.bash_aliases /home/svankina/.bash_aliases
+ln -s $HOME/wksp/dotfiles/.bash_personal /home/svankina/.bash_personal
 
 
