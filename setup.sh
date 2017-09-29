36039d9a15d7741c6e4767ad81a4d4dd327251d6
Sravan Vankina <spvankina@gmail.com>
2017-09-29 13:29:44 -0400

    improved setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 mv ~/.bashrc ~/bashrc_bak
+mv ~/.zshrc ~/zshrc_bak
+mkdir -p ~/.config/i3
 ln -s /home/svankina/wksp/dotfiles/.bash_aliases /home/svankina/
 ln -s /home/svankina/wksp/dotfiles/.bashrc /home/svankina/
 ln -s /home/svankina/wksp/dotfiles/.pdbrc /home/svankina/
@@ -7,3 +9,5 @@ ln -s /home/svankina/wksp/dotfiles/.tmux.conf /home/svankina/
 ln -s /home/svankina/wksp/dotfiles/.vimrc /home/svankina/
 ln -s /home/svankina/wksp/dotfiles/.xinitrc /home/svankina/
 ln -s /home/svankina/wksp/dotfiles/.zshrc /home/svankina/
+ln -s /home/svankina/wksp/dotfiles/.config/i3/config /home/svankina/.config/i3/
+touch /home/svankina/.bash_personal
