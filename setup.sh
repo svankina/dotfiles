c316ed384b3ced905b3cfc89cba3630a01036f9b
Sravan Vankina <spvankina@gmail.com>
2023-08-29 13:13:27 +0530

    creating i3 config directory

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ ln -s ~/src/dotfiles/vimrc ~/.vimrc
 ln -s ~/src/dotfiles/xinitrc ~/.xinitrc
 rm ~/.zshrc
 ln -s ~/src/dotfiles/zshrc ~/.zshrc
+mkdir -p ~/.config/i3
 ln -s ~/src/dotfiles/config/i3/config ~/.config/i3/config
 touch ~/.bash_personal
